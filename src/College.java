12d0054cd75615ea157244d6e7727c0bf64dab12
MANOJ <MANOJ@192.168.0.126>
2019-01-07 13:10:53 +0530

    department,students

---
+++ src/College.java
@@ -0,0 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+class College {
+
+    String collegename;
+    List<Department> departments = new ArrayList<>();
+
+    public List<Department> createDepartment() throws IOException, ParseException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 3; i++) {
+            Department department = new Department();
+            System.out.println("enter Department");
+            while (true) {
+                department.departmentname = in.readLine();
+                if (department.departmentname.length() <= 50) {
+                    departments.add(department);
+                    department.createStudent();
+                    break;
+                } else {
+                    System.out.println("Enter valid department");
+                }
+
+                break;
+
+            }
+        }
+
+        return departments;
+    }
+
+    public static void main(String as[]) throws IOException, ParseException {
+        List<Department> department1 = new ArrayList<>();
+        College college = new College();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("enter College name");
+        while (true) {
+            college.collegename = in.readLine();
+
+            if (college.collegename.length() <= 50) {
+                department1 = college.createDepartment();
+                break;
+            } else {
+                System.out.println("enter valid Collegename:");
+            }
+
+        }
+    }
+
+}
---
+++ src/Student.java
@@ -0,0 +1,15 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+public class Student {
+
+    char[] studentname = new char[15];
+    Date dob;
+    String email;
+
+}
